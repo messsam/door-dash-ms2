6468ec6279cf775148f0472e82bedf4eea84f935
a-hany420 <ahmedhany6010@gmail.com>
2026-04-30 20:56:43 +0300

    finalized the code

--- src/game/engine/cells/DoorCell.java
+++ src/game/engine/cells/DoorCell.java
@@ -5,30 +5,30 @@ import game.engine.monsters.Monster;
 import game.engine.interfaces.CanisterModifier;
 
 public class DoorCell extends Cell implements CanisterModifier {
-	private Role role;
-	private int energy;
-	private boolean activated;
-	
-	public DoorCell(String name, Role role, int energy) {
-		super(name);
-		this.role = role;
-		this.energy = energy;
-		// activated is set to false by default.
-	}
-	
-	public Role getRole() { return role; }
-    public int getEnergy() { return energy; }
-    
-	public boolean isActivated() { return activated; }
-	public void setActivated(boolean activated) { this.activated = activated; }
+    private Role role;
+    private int energy;
+    private boolean activated;
 
-	public void modifyCanisterEnergy(Monster monster, int canisterValue) {
-		if (monster.isShielded()==true && canisterValue < 0){ 
-			monster.setShielded(false);}
-		else {
-			monster.setEnergy(monster.getEnergy()+canisterValue);
-		}
-	}
+    public DoorCell(String name, Role role, int energy) {
+        super(name);
+        this.role = role;
+        this.energy = energy;
+    }
+
+    public Role getRole() { return role; }
+    public int getEnergy() { return energy; }
+    public boolean isActivated() { return activated; }
+    public void setActivated(boolean activated) { this.activated = activated; }
+
+
+	@Override
+    public void modifyCanisterEnergy(Monster monster, int canisterValue) {
+        if (this.getRole() == monster.getRole()) {
+            monster.alterEnergy(canisterValue);
+        } else {
+        	monster.alterEnergy(-canisterValue);
+        }
+    }
 	
 	@Override
 	public void onLand(Monster landingMonster, Monster opponentMonster) {
@@ -40,33 +40,19 @@ public class DoorCell extends Cell implements CanisterModifier {
 			boolean flag=false;
 			int energyFirst=0;
 			int energyEnd=0;
-			if(this.getRole()==landingMonster.getRole()){
-				energyFirst=landingMonster.getEnergy();
-				modifyCanisterEnergy(landingMonster,this.getEnergy());
-				energyEnd=landingMonster.getEnergy();
-				if(energyFirst!=energyEnd){flag=true;}
-			}
-			else if(this.getRole()!=landingMonster.getRole()){
-				energyFirst=landingMonster.getEnergy();
-				modifyCanisterEnergy(landingMonster,-1 * this.getEnergy());
-				energyEnd=landingMonster.getEnergy();
-				if(energyFirst!=energyEnd){flag=true;}
-			}
+			energyFirst=landingMonster.getEnergy();
+			this.modifyCanisterEnergy(landingMonster,this.getEnergy());
+			energyEnd=landingMonster.getEnergy();
+			if(energyFirst==energyEnd){return;}
+			else if(energyFirst!=energyEnd){flag=true;}
 			for(int i=0; i<Board.getStationedMonsters().size();i++){
 				Monster stationed=Board.getStationedMonsters().get(i);
 				if(stationed.getRole()==landingMonster.getRole()){
-					if(this.getRole()==stationed.getRole()){
-						energyFirst=stationed.getEnergy();
-						modifyCanisterEnergy(stationed,this.getEnergy());
-						energyEnd=stationed.getEnergy();
-						if(energyFirst!=energyEnd){flag=true;}
-					}
-					else if(this.getRole()!=stationed.getRole()){
-						energyFirst=stationed.getEnergy();
-						modifyCanisterEnergy(stationed,-1 * this.getEnergy());
-						energyEnd=stationed.getEnergy();
-						if(energyFirst!=energyEnd){flag=true;}
-					}
+				energyFirst=stationed.getEnergy();
+				this.modifyCanisterEnergy(stationed,this.getEnergy());
+				energyEnd=stationed.getEnergy();
+				if(energyFirst!=energyEnd){flag=true;}
+					
 				}
 			}
 			if(flag==true){
